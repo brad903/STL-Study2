4cb527cd98cff353b80a4b3d3b6224f8b4212130
Brad Lee <brad903@naver.com>
2018-07-27 10:59:14 +0900

    fix bugs
    - change initial value of result
    - reduce unecessary code

--- C++ source/ColorfulBoxesAndBalls.cpp
+++ C++ source/ColorfulBoxesAndBalls.cpp
@@ -1,17 +1,15 @@
 #include <algorithm>
+#include <limits.h>
 using namespace std;
 
     class ColorfulBoxesAndBalls {
         public:
         int getMaximum(int numRed,int numBlue,int onlyRed, int onlyBlue, int bothColors){
-            int result = 0;
+            int result = INT_MIN;
             int numBoth = min(numRed, numBlue);
-            
-            // 경우의 수 1: onlyRed, onlyBlue만 있는 경우
-            result = onlyRed*numRed + onlyBlue*numBlue;
 
-            // 경우의 수 2: 혼합이 존재하는 경우
-            for(int i=1; i<=numBoth; i++){
+            // 경우의 수:  onlyRed, onlyBlue만 있는 경우 ~ 혼합이 존재하는 경우
+            for(int i=0; i<=numBoth; i++){
                 int temp = onlyRed*(numRed-i) + onlyBlue*(numBlue-i) + bothColors*2*i;  // 하나 바꿀때마다 bothColor 개수가 2배씩 늘어남
                 result = max(temp, result);
             }
@@ -23,10 +21,10 @@ using namespace std;
 int main(){
     ColorfulBoxesAndBalls CBB;
     // int x = CBB.getMaximum(2, 3, 100, 400, 200);
-    // int x = CBB.getMaximum(2, 3, 100, 400, 300);
+    int x = CBB.getMaximum(2, 3, 100, 400, 300);
     // int x = CBB.getMaximum(5, 5, 464, 464, 464);
     // int x = CBB.getMaximum(1, 4, 20, -30, -10);
-    int x = CBB.getMaximum(9, 1, -1, -10, 4);  // 이 경우 맞지않음
+    // int x = CBB.getMaximum(9, 1, -1, -10, 4);
     printf("정답 : %d\n", x);
     return 0;
 }
