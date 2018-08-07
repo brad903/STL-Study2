c01032e9154f150c63f7169d202de2a4e1ddbf06
Brad Lee <brad903@naver.com>
2018-08-07 15:28:17 +0900

    해밀턴 경로 책 문제풀이

--- java source/src/HamiltonPath.java
+++ java source/src/HamiltonPath.java
@@ -1,60 +1,63 @@
-import java.util.ArrayList;
-
 public class HamiltonPath {
-	private int cityCount;
-	private int[][] visit;
-	private final String[] roads;
-	ArrayList<ArrayList<Integer>> chunk = new ArrayList<>();
+	String[] roads;
+	boolean[] visited;
 
-	HamiltonPath(String[] roads) {
-		cityCount = roads.length;
-		visit = new int[cityCount][cityCount]; // 방문여부 표시
-		this.roads = roads;
-	}
-
-	public int visitCity(int i) {
-		for (int j = 0; j < cityCount; j++) {
-			if (visit[j][i] != 0 && roads[j].split("")[i].equals("N")) {
-				System.out.println("if문 " + i + ", " + j);
-				visit[i][j] = 1; // 방문 표시
-
-				String temp = roads[i].split("")[j];
-				if (temp.equals("Y")) {
-					System.out.println(j + "호출");
-					visitCity(j);
-					return j;   
-				}
-			} else {
-				visit[i][j] = 1;
-				System.out.println("else문 " + i + ", " + j);
-				continue; // continue 넣으니까 다양해졌다 훨
-			}
+	void dfs(int city) {
+		visited[city] = true;
+		for (int i = 0; i < roads[city].length(); i++) {
+			if (roads[city].charAt(i) == 'Y' && !visited[i])
+				dfs(i);
 		}
-		return -1;
 	}
 
 	public int countPaths(String[] roads) {
-		for (int i = 0; i < cityCount; i++) {
-			ArrayList<Integer> member = new ArrayList<>();
-			if (visit[i][0] == 0)
-				member.add(i);
-			int visitResult = visitCity(i);
-			if (visitResult != -1) {
-				System.out.println(visitResult + "추가");
-				member.add(visitResult);
+		int group = 0, free = 0;
+
+		int[] connect = new int[roads.length];
+		long sum = 1;
+
+		this.roads = roads;
+		visited = new boolean[roads.length];
+
+		for (int i = 0; i < roads.length; i++) { // Y가 몇 개 연결되었는지 조사
+			int y = 0;
+			for (int j = 0; j < roads[i].length(); j++) {
+				if (roads[i].charAt(j) == 'Y') {
+					if (2 < ++y)
+						return 0;
+				}
 			}
-			if (member.isEmpty() == false)
-				chunk.add(member);
+			connect[i] = y;
 		}
 
-		System.out.println(chunk);
+		for (int i = 0; i < connect.length; i++) {
+			if (connect[i] == 0) {
+				visited[i] = true;
+				free++;
+			} else if (connect[i] == 1 && !visited[i]) {
+				group++;
+				dfs(i);
+			}
+		}
 
-		return 0;
+		// 왜 함수 안에 다른 함수를 넣을 수 없을까?
+
+		for (int i = 0; i < visited.length; i++) { // 순환하는 경우
+			if (!visited[i])
+				return 0;
+		}
+		for (int i = 0; i < group + free; i++) {
+			sum = sum * (i + 1) % 1000000007;
+		}
+		for (int i = 0; i < group; i++) {
+			sum = sum * 2 % 1000000007;
+		}
+		return (int)sum;
 	}
 
 	public static void main(String[] args) {
 		String[] roads = { "NNNNNY", "NNNNYN", "NNNNYN", "NNNNNN", "NYYNNN", "YNNNNN" };
-		HamiltonPath HP = new HamiltonPath(roads);
-		HP.countPaths(roads);
+		HamiltonPath HP = new HamiltonPath();
+		System.out.println(HP.countPaths(roads));
 	}
 }
