9984031b1b9810315d5590e9ad47b1a8ca0ab2ba
Brad Lee <brad903@naver.com>
2018-09-18 17:18:53 +0900

    clean modified log

--- C++ source/CrazyBot_new.cpp
+++ C++ source/CrazyBot_new.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 bool D[29][29];
-double V[29][29];
 
 class CrazyBot {
     double E,W,S,N;
@@ -14,41 +13,32 @@ class CrazyBot {
         W = (double)west/100;
         S = (double)south/100;
         N = (double)north/100;
-        V[14][14] = 1;
 
-        return dfs(0, 14, 14, -1, -1);
+        return dfs(0, 14, 14);
     }
 
-    double dfs(int n, int x, int y, int a, int b){  // a:0→서에서옴, b:0->남에서 옴, -1: 변화없음
-        if(D[x][y] != 0) return 0;  // 기존에 방문한 곳이면 Out
+    double dfs(int n, int x, int y){  // a:0→서에서옴, b:0->남에서 옴, -1: 변화없음
+        if(D[x][y]) return 0;  // 기존에 방문한 곳이면 Out
+        if(n == END) {
+            return 1;  // 1*W*E... 이렇게 계산됨
+        }
 
         D[x][y] = 1;  // 새로운 곳 방문 표시
+
+        double ret = 0;
+        ret += dfs(n+1, x-1, y) * W + dfs(n+1, x+1, y) * E + dfs(n+1, x, y+1) * N + dfs(n+1, x, y-1) * S;
+
+        D[x][y] = 0;  // 기존 지나온 경로 초기화
         
-        if(a == 0){  // 서에서 옴
-            V[x][y] = V[x-1][y] * E;
-        }else if(a == 1){  // 동에서 옴
-            V[x][y] = V[x+1][y] * W;
-        }
-
-        if(b == 0){  // 남에서 옴
-            V[x][y] = V[x][y-1] * N;
-        }else if(b ==1){
-            V[x][y] = V[x][y+1] * S;
-        }
-
-        printf("%d, %d, %d, %d, %d, %f\n", n, x, y, a, b, V[x][y]);
-
-        if(n == END) return V[x][y];
-        
-        return dfs(n+1, x-1, y, 1, -1) + dfs(n+1, x+1, y, 0, -1) + dfs(n+1, x, y+1, -1, 0) + dfs(n+1, x, y-1, -1, 1);
+        return ret;
     }
 };
 
 int main(){
     CrazyBot CB;
-    // double x = CB.getProbability(1, 25, 25, 25, 25);
+    double x = CB.getProbability(1, 25, 25, 25, 25);
     // double x = CB.getProbability(2, 25, 25, 25, 25);
-    double x = CB.getProbability(7, 50, 0, 0, 50);
+    // double x = CB.getProbability(7, 50, 0, 0, 50);
 
     printf("%f\n", x);
 
