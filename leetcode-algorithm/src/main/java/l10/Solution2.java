e804d0e60567e66c4500b26f135123e87edf0ffa
Brad Lee <brad903@naver.com>
2018-11-27 20:08:15 +0900

    leetcode l10) refereced another solution

---
+++ leetcode-algorithm/src/main/java/l10/Solution2.java
@@ -0,0 +1,25 @@
+package l10;
+
+public class Solution2 {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxHeight = 0;
+        int answer = 0;
+        while(left < right) {
+            maxHeight = Math.min(height[left], height[right]);
+            answer = Math.max(answer, (right - left) * maxHeight);
+
+            if(height[left] == height[right]) {
+                left++;
+                right--;
+            } else if(height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return answer;
+    }
+}
--- leetcode-algorithm/src/test/java/l11/SolutionTest.java
+++ leetcode-algorithm/src/test/java/l11/SolutionTest.java
@@ -1,5 +1,6 @@
 package l11;
 
+import l10.Solution2;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionTest {
     Solution solution = new Solution();
+    Solution2 solution2 = new Solution2();
 
     @Test
     public void test1() {
         int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
         assertThat(solution.maxArea(height)).isEqualTo(49);
+    }
+
+    @Test
+    public void test2() {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        assertThat(solution2.maxArea(height)).isEqualTo(49);
     }
 }
